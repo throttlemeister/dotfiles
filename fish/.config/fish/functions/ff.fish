395ab200d622b2c496fc819bff1ed70a4af245da
throttlemeister <throttlemeister@sport-touring.eu>
2024-09-18 09:19:34 +0200

    Updated logo size for ff.fish on konsole

--- fish/.config/fish/functions/ff.fish
+++ fish/.config/fish/functions/ff.fish
@@ -1,11 +1,11 @@
 function ff -d 'fastfetch shortcut'
-  if type --quiet fastfetch
-    if test -n "$ALACRITTY_WINDOW_ID"
-      fastfetch -l opensuse $argv
+    if type --quiet fastfetch
+        if test -n "$ALACRITTY_WINDOW_ID"
+            fastfetch -l opensuse $argv
+        else
+            fastfetch -l ~/ansible/files/twgrey.png --logo-type iterm --logo-padding-top 2 --logo-width 45 $argv
+        end
     else
-      fastfetch -l ~/ansible/files/twgrey.png --logo-type iterm --logo-padding-top 2 $argv
+        command neofetch $argv
     end
-  else
-    command neofetch $argv
-  end
 end
