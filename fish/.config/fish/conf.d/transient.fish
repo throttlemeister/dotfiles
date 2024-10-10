51977644a53cc9d7e7b7358f9747cfa920cd4362
throttlemeister <throttlemeister@sport-touring.eu>
2024-10-11 00:44:29 +0200

    Updated plugins

--- fish/.config/fish/conf.d/transient.fish
+++ fish/.config/fish/conf.d/transient.fish
@@ -42,23 +42,17 @@ function __transient_execute
     #     - 0: valid and complete
     #     - 1: handle type "\[enter]"
     #     - 2: The empty commandline is an error, not incomplete
-    commandline --is-valid
-    set -l valid $status
-    if test $valid -eq 1
-        and test (string match -r '.*\\\$' -- (commandline --current-buffer)) # check like "ls \"
-        or test $valid -eq 2
-        or commandline --paging-full-mode
-        commandline -f execute
+    if commandline --is-valid || test -z "$(commandline)" && not commandline --paging-mode
+        set --global TRANSIENT transient
+        commandline --function expand-abbr suppress-autosuggestion repaint execute
         return 0
     end
-    set --global TRANSIENT transient
-    commandline --function expand-abbr suppress-autosuggestion repaint execute
+    commandline -f execute
 end
 
 function __transient_ctrl_c_execute
     set --global TRANSIENT transient
     if test "$(commandline --current-buffer)" = ""
-        commandline --function repaint execute
         return 0
     end
 
