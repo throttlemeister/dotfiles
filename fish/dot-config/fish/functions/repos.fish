48a36d9f9acc1d1f0b3e43db7152a85c97ce4ee0
throttlemeister <throttlemeister@sport-touring.eu>
2024-11-05 18:31:54 +0100

    Add function to sync all git repos at once

---
+++ fish/dot-config/fish/functions/repos.fish
@@ -0,0 +1,18 @@
+# Function to download and synchornize all git repositories
+#
+function repos -d "This function will synchornize all git repositories"
+    set _oldpath $PWD
+    echo "Pull dotfiles repo.."
+    cd ~/.dotfiles/
+    pull
+    echo "Pull ansible repo.."
+    cd ~/ansible/
+    pull
+    echo "Pull crashdot.com repo.."
+    cd ~/web/crashdot.com/
+    pull
+    echo "Pull sport-touring.eu repo.."
+    cd ~/web/sport-touring.eu/
+    pull
+    cd $_oldpath
+end
