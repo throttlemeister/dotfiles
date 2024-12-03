3efa7d9de9e6a1971a84a6aadc9bdc3afb889ce7
throttlemeister <throttlemeister@sport-touring.eu>
2024-12-03 09:22:01 +0100

    Changed np.fish to accept commandline parameter directly

--- fish/dot-config/fish/functions/np.fish
+++ fish/dot-config/fish/functions/np.fish
@@ -1,7 +1,11 @@
 function np
-    echo "Existing direcotories / catagories under posts:"
-    ls -D content/posts
-    echo ""
-    read -P "Enter path and file for the article to create, relative from the posts directory: " article
-    hugo new content content/posts/$article
+    if not set -q argv[1]
+        echo "Existing direcotories / catagories under posts:"
+        ls -D content/posts
+        echo ""
+        read -P "Enter path and file for the article to create, relative from the posts directory: " article
+        hugo new content content/posts/$article
+    else
+        hugo new content content/posts/$argv
+    end
 end
