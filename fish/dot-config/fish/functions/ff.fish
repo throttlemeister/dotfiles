b652c1cd8db202dbec3af344ecabea8657e0fad0
throttlemeister <throttlemeister@sport-touring.eu>
2024-12-20 11:17:20 +0100

    Formatting

--- fish/dot-config/fish/functions/ff.fish
+++ fish/dot-config/fish/functions/ff.fish
@@ -1,15 +1,15 @@
 function ff -d 'fastfetch shortcut'
-    if type --quiet fastfetch
-        if test -n "$ALACRITTY_WINDOW_ID"
-            fastfetch -l opensuse -c examples/6.jsonc $argv
-        else
-            if test -n "$KITTY_WINDOW_ID"
-                fastfetch -l opensuse -c examples/6.jsonc $argv
-            else
-                fastfetch -l ~/ansible/files/twgrey.png --logo-type iterm --logo-padding-top 2 --logo-width 45 -c examples/6.jsonc $argv
-            end
-        end
+  if type --quiet fastfetch
+    if test -n "$ALACRITTY_WINDOW_ID"
+      fastfetch -l opensuse -c examples/6.jsonc $argv
     else
-        command neofetch $argv
+      if test -n "$KITTY_WINDOW_ID"
+        fastfetch -l opensuse -c examples/6.jsonc $argv
+      else
+        fastfetch -l ~/ansible/files/twgrey.png --logo-type iterm --logo-padding-top 2 --logo-width 45 -c examples/6.jsonc $argv
+      end
     end
+  else
+    command neofetch $argv
+  end
 end
