c23a7acd8994826261eef38c4a8bd9715cf43d8c
throttlemeister <throttlemeister@sport-touring.eu>
2024-12-28 14:00:38 +0100

    Removed custom logo so the new Tumbleweed logo can be used

--- fish/dot-config/fish/functions/ff.fish
+++ fish/dot-config/fish/functions/ff.fish
@@ -1,15 +1,16 @@
 function ff -d 'fastfetch shortcut'
-  if type --quiet fastfetch
-    if test -n "$ALACRITTY_WINDOW_ID"
-      fastfetch -l opensuse -c examples/6.jsonc $argv
+    if type --quiet fastfetch
+        if test -n "$ALACRITTY_WINDOW_ID"
+            fastfetch -c examples/6.jsonc $argv
+        else
+            if test -n "$KITTY_WINDOW_ID"
+                fastfetch -c examples/6.jsonc $argv
+            else
+                fastfetch -l ~/ansible/files/twgrey.png --logo-type iterm --logo-padding-top 2 --logo-width 45 -c examples/6.jsonc $argv
+            end
+        end
     else
-      if test -n "$KITTY_WINDOW_ID"
-        fastfetch -l opensuse -c examples/6.jsonc $argv
-      else
-        fastfetch -l ~/ansible/files/twgrey.png --logo-type iterm --logo-padding-top 2 --logo-width 45 -c examples/6.jsonc $argv
-      end
+        command neofetch $argv
     end
-  else
-    command neofetch $argv
-  end
 end
+
