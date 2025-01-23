be07aa9349fe489fa3d94ec9389be6da19bbdb12
throttlemeister <throttlemeister@sport-touring.eu>
2025-01-23 23:29:24 +0100

    Updated zypper to include --clean-deps flag when removing packages

--- fish/dot-config/fish/functions/zypper.fish
+++ fish/dot-config/fish/functions/zypper.fish
@@ -1,8 +1,13 @@
 # Defined in - @ line 1
 function zypper --wraps='sudo zypper'
-	if test $argv[1] = up
-		sudo zypper dup
-	else
-		sudo zypper $argv;
-	end
+    if test $argv[1] = up
+        sudo zypper dup
+    else
+        if test $argv[1] = rm
+            sudo zypper rm --clean-deps argv[2]
+        else
+            sudo zypper $argv
+        end
+    end
 end
+
