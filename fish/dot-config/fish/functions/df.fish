ad6d135fd1b058c3f3794d84f3857083e0eae68c
throttlemeister <throttlemeister@sport-touring.eu>
2025-05-04 01:10:19 +0200

    Added option to only show local using duf.

--- fish/dot-config/fish/functions/df.fish
+++ fish/dot-config/fish/functions/df.fish
@@ -1,6 +1,11 @@
 function df -d "Using duf instead of df when we can"
+    argparse h -- $argv
+    or return
+    if set -ql _flag_h
+        set __param --only local
+    end
     if command -sq duf
-        duf
+        duf $__param
     else
         df $argv
     end
