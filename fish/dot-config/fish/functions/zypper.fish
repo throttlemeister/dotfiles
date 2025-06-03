48e4d16cd91acbd9a36e221b9cb8c003480f1ecc
throttlemeister <throttlemeister@sport-touring.eu>
2025-06-03 18:47:21 +0200

    Updated commands now that the new zypper speedups are default

--- fish/dot-config/fish/functions/zypper.fish
+++ fish/dot-config/fish/functions/zypper.fish
@@ -1,13 +1,13 @@
 function zypper --wraps='sudo zypper'
     switch $argv[1]
         case up
-            sudo env ZYPP_PCK_PRELOAD=1 zypper dup
+            sudo zypper dup
         case dup
-            sudo env ZYPP_PCK_PRELOAD=1 zypper $argv
+            sudo zypper $argv
         case rm
             sudo zypper rm --clean-deps $argv[2]
         case ref
-            sudo env ZYPP_CURL2=1 zypper $argv
+            sudo zypper $argv
         case '*'
             sudo zypper $argv
     end
