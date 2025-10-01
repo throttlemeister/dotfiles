0123b5eac166bceb882ef2087c63116876d9a676
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-01 22:31:03 +0200

    Fixed oopsie, ie moved linter config for markdown files to seperate file.

--- nvim/dot-config/nvim/lua/plugins/render-markdown.lua
+++ nvim/dot-config/nvim/lua/plugins/render-markdown.lua
@@ -161,14 +161,4 @@ return {
       end,
     }):map("<leader>um")
   end,
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters = {
-        ["markdownlint-cli2"] = {
-          args = { "--config", vim.fn.expand("$HOME/.markdownlint-cli2.yaml"), "--" },
-        },
-      },
-    },
-  },
 }
