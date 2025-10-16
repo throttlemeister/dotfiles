29a8d92d3dc01a4811050a9c1f8b5919472a4a2a
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-17 00:20:35 +0200

    Missed the step indicator, added

--- fish/dot-config/fish/functions/repos.fish
+++ fish/dot-config/fish/functions/repos.fish
@@ -3,24 +3,24 @@
 function repos -d "This function will synchornize all git repositories"
     echo -ne "Synchronizing repositories \n"
     set _oldpath $PWD
-    echo -ne "Pullinging dotfiles            [███████..................................] \r"
+    echo -ne "Pullinging dotfiles         1/7[███████..................................] \r"
     cd ~/.dotfiles/
     pull >/dev/null 2>&1
-    echo -ne "Pullinging ansible             [█████████████............................] \r"
+    echo -ne "Pullinging ansible          2/7[█████████████............................] \r"
     cd ~/ansible/
     pull >/dev/null 2>&1
-    echo -ne "Pulling notes                  [████████████████████.....................] \r"
+    echo -ne "Pulling notes               3/7[████████████████████.....................] \r"
     cd ~/notes/
     pull >/dev/null 2>&1
-    echo -ne "Pulling wallpaper              [███████████████████████████..............] \r"
+    echo -ne "Pulling wallpaper           4/7[███████████████████████████..............] \r"
     cd ~/Pictures/wallpapers/
     pull >/dev/null 2>&1
-    echo -ne "Pulling crashdot.com           [██████████████████████████████████.......] \r"
+    echo -ne "Pulling crashdot.com        5/7[██████████████████████████████████.......] \r"
     cd ~/web/crashdot.com/
     pull >/dev/null 2>&1
-    echo -ne "Pulling sport-touring.eu       [█████████████████████████████████████████] \r"
+    echo -ne "Pulling sport-touring.eu    6/7[█████████████████████████████████████████] \r"
     cd ~/web/sport-touring.eu/
     pull >/dev/null 2>&1
     cd $_oldpath
-    echo -ne "Done                           [█████████████████████████████████████████] \r"
+    echo -ne "Done                        7/7[█████████████████████████████████████████] \r"
 end
