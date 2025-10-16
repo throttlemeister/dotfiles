e11769e23e9e35d738b3798de16b23be4075e270
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-16 23:34:14 +0200

    Added progress bar

--- fish/dot-config/fish/functions/repos.fish
+++ fish/dot-config/fish/functions/repos.fish
@@ -2,23 +2,24 @@
 #
 function repos -d "This function will synchornize all git repositories"
     set _oldpath $PWD
-    echo -ne "Pulling dotfiles........ \r"
+    echo -ne "Pullinging dotfiles            [███...............] \r"
     cd ~/.dotfiles/
     pull >/dev/null 2>&1
-    echo -ne "Pulling ansible......... \r"
+    echo -ne "Pullinging ansible             [█████.............] \r"
     cd ~/ansible/
     pull >/dev/null 2>&1
-    echo -ne "Pulling notes........... \r"
+    echo -ne "Pulling notes                  [█████████.........] \r"
     cd ~/notes/
     pull >/dev/null 2>&1
-    echo -ne "Pulling wallpaper....... \r"
+    echo -ne "Pulling wallpaper              [████████████......] \r"
     cd ~/Pictures/wallpapers/
     pull >/dev/null 2>&1
-    echo -ne "Pulling crashdot.com.... \r"
+    echo -ne "Pulling crashdot.com           [███████████████...} \r"
     cd ~/web/crashdot.com/
     pull >/dev/null 2>&1
-    echo -ne "Pulling sport-touring.eu \r"
+    echo -ne "Pulling sport-touring.eu       [██████████████████] \r"
     cd ~/web/sport-touring.eu/
     pull >/dev/null 2>&1
     cd $_oldpath
+    echo -ne "Done                           [██████████████████] \n"
 end
