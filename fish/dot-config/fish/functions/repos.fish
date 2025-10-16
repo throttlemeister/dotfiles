5ff9eb66dd36cd533bb187c8bfa591d8cfe6dfd0
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-17 00:16:20 +0200

    Added the wider progressbar

--- fish/dot-config/fish/functions/repos.fish
+++ fish/dot-config/fish/functions/repos.fish
@@ -3,24 +3,24 @@
 function repos -d "This function will synchornize all git repositories"
     echo -ne "Synchronizing repositories \n"
     set _oldpath $PWD
-    echo -ne "Pullinging dotfiles            [███...............] \r"
+    echo -ne "Pullinging dotfiles            [███████..................................] \r"
     cd ~/.dotfiles/
     pull >/dev/null 2>&1
-    echo -ne "Pullinging ansible             [█████.............] \r"
+    echo -ne "Pullinging ansible             [█████████████............................] \r"
     cd ~/ansible/
     pull >/dev/null 2>&1
-    echo -ne "Pulling notes                  [█████████.........] \r"
+    echo -ne "Pulling notes                  [████████████████████.....................] \r"
     cd ~/notes/
     pull >/dev/null 2>&1
-    echo -ne "Pulling wallpaper              [████████████......] \r"
+    echo -ne "Pulling wallpaper              [███████████████████████████..............] \r"
     cd ~/Pictures/wallpapers/
     pull >/dev/null 2>&1
-    echo -ne "Pulling crashdot.com           [███████████████...} \r"
+    echo -ne "Pulling crashdot.com           [██████████████████████████████████.......] \r"
     cd ~/web/crashdot.com/
     pull >/dev/null 2>&1
-    echo -ne "Pulling sport-touring.eu       [██████████████████] \r"
+    echo -ne "Pulling sport-touring.eu       [█████████████████████████████████████████] \r"
     cd ~/web/sport-touring.eu/
     pull >/dev/null 2>&1
     cd $_oldpath
-    echo -ne "Done                           [██████████████████] \n"
+    echo -ne "Done                           [█████████████████████████████████████████] \r"
 end
