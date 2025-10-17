0820921b4b03f57495e14f6f2b334329beb31e00
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-18 00:11:55 +0200

    Added check to only synchornize configurations once an hour

--- fish/dot-config/fish/functions/fish_greeting.fish
+++ fish/dot-config/fish/functions/fish_greeting.fish
@@ -18,46 +18,51 @@ function fish_greeting
         if [ -z $__link ] 2>/dev/null
             echo -e "\nNo network detected; skipping configuration checks...\n"
         else
-            echo -ne "\nSynchronizing configuration \n"
-            sleep 0.5
-            if test -d $HOME/.dotfiles/.git
-                set _oldpath $PWD
-                cd $HOME/.dotfiles
-                echo -ne "Syncing dotfiles            1/2[██████████████...........................] \r"
-                git pull >/dev/null 2>&1
-                cd $_oldpath
-            end
-            #            # We do that for the Ansible directory too
-            #            if test -d $HOME/ansible/.git
-            #                set _oldpath $PWD
-            #                cd $HOME/ansible
-            #                echo "Updating ansible"
-            #                git pull 1>/dev/null
-            #                cd
-            #                cd $_oldpath
-            #            else
-            #                set _oldpath $PWD
-            #                cd $HOME
-            #                git clone https://github.com/throttlemeister/ansible.git
-            #                cd $_oldpath
-            #            end
-            # And for our notes directory
-            if test -d $HOME/notes
-                set _oldpath $PWD
-                cd $HOME/notes
-                echo -ne "Syncing notes               2/2[████████████████████████████.............] \r"
-                git pull >/dev/null 2>&1
-                cd
-                cd $_oldpath
+            if test -z (find ~/.gitcheck -mmin -60)
+                echo -ne "\nSynchronizing configuration \n"
+                sleep 0.5
+                if test -d $HOME/.dotfiles/.git
+                    set _oldpath $PWD
+                    cd $HOME/.dotfiles
+                    echo -ne "Syncing dotfiles            1/2[██████████████...........................] \r"
+                    git pull >/dev/null 2>&1
+                    cd $_oldpath
+                end
+                #            # We do that for the Ansible directory too
+                #            if test -d $HOME/ansible/.git
+                #                set _oldpath $PWD
+                #                cd $HOME/ansible
+                #                echo "Updating ansible"
+                #                git pull 1>/dev/null
+                #                cd
+                #                cd $_oldpath
+                #            else
+                #                set _oldpath $PWD
+                #                cd $HOME
+                #                git clone https://github.com/throttlemeister/ansible.git
+                #                cd $_oldpath
+                #            end
+                # And for our notes directory
+                if test -d $HOME/notes
+                    set _oldpath $PWD
+                    cd $HOME/notes
+                    echo -ne "Syncing notes               2/2[████████████████████████████.............] \r"
+                    git pull >/dev/null 2>&1
+                    cd
+                    cd $_oldpath
+                else
+                    set _oldpath $PWD
+                    cd $HOME
+                    echo "Notes not found. Cloning from GitHub"
+                    github notes
+                    cd $_oldpath
+                end
+                echo -ne "Done                        2/2[█████████████████████████████████████████] \n"
+                echo -ne "\n"
+                touch ~/.gitcheck
             else
-                set _oldpath $PWD
-                cd $HOME
-                echo "Notes not found. Cloning from GitHub"
-                github notes
-                cd $_oldpath
+                echo -ne "No refresh required"
             end
-            echo -ne "Done                        2/2[█████████████████████████████████████████] \n"
-            echo -ne "\n"
         end
         # GitHub way of authenticating changed, so we do not use this anymore.
         # Housekeeping.
