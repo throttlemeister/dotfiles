bc385ba72509beee8c70fef2a6263566c1efe7d3
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-26 00:35:11 +0200

    Changed bat theme to auto

--- fish/dot-config/fish/functions/cat.fish
+++ fish/dot-config/fish/functions/cat.fish
@@ -1,8 +1,7 @@
 function cat --wraps='batcat' --description 'Use batcat if available, and ls if arg is dir'
-    __fish_dynamic_alias cat "bat -P --theme=ansi" "batcat -P --theme=ansi" cat % $argv
+    __fish_dynamic_alias cat "bat -P --theme=auto" "batcat -P --theme=auto" cat % $argv
     if test $status -ne 0
         # Try to `ls` if the argument is a directory
         ls -l $argv 2>/dev/null
     end
 end
-
