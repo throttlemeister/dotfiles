f690b62311c79b1d96128afad140622ff18d72e8
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-26 23:01:14 +0100

    More tidying up for ls functions

--- fish/dot-config/fish/conf.d/ls_aliases.fish
+++ fish/dot-config/fish/conf.d/ls_aliases.fish
@@ -1,24 +1,22 @@
-function ls -d 'eza instead of ls'
-    set -f _eflags "-g --header --group-directories-first --git --icons=auto"
+# Set global variable for common eza/exa flags
+set -g _eflags "-g --header --group-directories-first --git --icons=auto"
+
+function ls -d 'Using eza/exa instead of ls when installed'
     __fish_dynamic_alias ls "eza $_eflags" "exa $_eflags" ls % $argv
 end
 
 function ll
-    set -f _eflags "-g --header --group-directories-first --git --icons=auto"
     __fish_dynamic_alias ls "eza $_eflags" "exa $_eflags" ls % -la $argv
 end
 
 function l
-    set -f _eflags "-g --header --group-directories-first --git --icons=auto"
     __fish_dynamic_alias ls "eza $_eflags" "exa $_eflags" ls % $argv
 end
 
 function lt
-    set -f _eflags "-g --header --group-directories-first --git --icons=auto"
-    __fish_dynamic_alias ls "eza -laa -snew $_eflags" "exa -laa -snew $_eflags" "ls -ltr" % $argv
+    __fish_dynamic_alias ls "eza -aa -snew $_eflags" "exa -aa -snew $_eflags" "ls -tr" % -l $argv
 end
 
 function lls
-    set -f _eflags "-g --header --group-directories-first --git --icons=auto"
     __fish_dynamic_alias ls "eza --sort=size $_eflags" "exa --sort=size $_eflags" "ls -hS" % -la $argv
 end
