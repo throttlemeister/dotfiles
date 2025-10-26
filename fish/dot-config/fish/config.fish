da9343918a31f630c6b51ffa8f3b987f81345a76
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-26 19:47:17 +0100

    Removed cd.fish and changed zoxide initialization to use cd as the execute command for it, replacing built-in cd automatically.

--- fish/dot-config/fish/config.fish
+++ fish/dot-config/fish/config.fish
@@ -1,6 +1,6 @@
 set -gx fish_prompt_pwd_dir_length 0
 set -gx ANSIBLE_CONFIG $HOME/ansible/ansible.cfg
-zoxide init fish | source
+zoxide init fish --cmd cd | source
 atuin init fish | source
 bind up _atuin_bind_up
 fzf_configure_bindings --directory=\cf --git_log=\cg
--- fish/dot-config/fish/functions/cd.fish
+++
@@ -1,7 +0,0 @@
-function cd -d 'Using zoxide as replacement for cd, if it exists'
-    if type --quiet zoxide
-        z $argv
-    else
-        command cd $argv
-    end
-end
