c792a95759143de043c78ff4a47de354064012fb
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-26 13:08:22 +0100

    Fixed typo

--- fish/dot-config/fish/functions/mv.fish
+++ fish/dot-config/fish/functions/mv.fish
@@ -1,3 +1,3 @@
 function mv --wraps=advmv --wraps=mv -d 'Use advmv instead of mv when possible'
-    __fish_dynamic_alias mv advmv mv % -p $argv
+    __fish_dynamic_alias mv "advmv -g" mv % $argv
 end
