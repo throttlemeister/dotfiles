ad2fc163826d1d13752dd369fc6df660b0d7e936
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-26 21:45:56 +0100

    Cleaned up ls aliases to be more readable

--- fish/dot-config/fish/conf.d/ls_aliases.fish
+++ fish/dot-config/fish/conf.d/ls_aliases.fish
@@ -1,19 +1,24 @@
 function ls -d 'eza instead of ls'
-    __fish_dynamic_alias ls "eza -g --header --group-directories-first --git --icons=auto" "exa -g --header --group-directories-first --git --icons=auto" ls % $argv
+    set -f _eflags "-g --header --group-directories-first --git --icons=auto"
+    __fish_dynamic_alias ls "eza $_eflags" "exa $_eflags" ls % $argv
 end
 
 function ll
-    __fish_dynamic_alias ls "eza -g --header --group-directories-first --git --icons=auto" "exa -g --header --group-directories-first --git --icons=auto" ls % -la $argv
+    set -f _eflags "-g --header --group-directories-first --git --icons=auto"
+    __fish_dynamic_alias ls "eza $_eflags" "exa $_eflags" ls % -la $argv
 end
 
 function l
-    __fish_dynamic_alias ls "eza --header --group-directories-first --git --icons=auto" "exa --header --group-directories-first --git --icons=auto" ls % $argv
+    set -f _eflags "-g --header --group-directories-first --git --icons=auto"
+    __fish_dynamic_alias ls "eza $_eflags" "exa $_eflags" ls % $argv
 end
 
 function lt
-    __fish_dynamic_alias ls "eza -laa -snew -g --header --group-directories-first --git --icons=auto" "exa -laa -snew -g --header --group-directories-first --git --icons=auto" "ls -ltr" % $argv
+    set -f _eflags "-g --header --group-directories-first --git --icons=auto"
+    __fish_dynamic_alias ls "eza -laa -snew $_eflags" "exa -laa -snew $_eflags" "ls -ltr" % $argv
 end
 
 function lls
-    __fish_dynamic_alias ls "eza -g --sort=size --header --group-directories-first --git --icons=auto" "exa -g --sort=size --header --group-directories-first --git --icons=auto" "ls -hS" % -la $argv
+    set -f _eflags "-g --header --group-directories-first --git --icons=auto"
+    __fish_dynamic_alias ls "eza --sort=size $_eflags" "exa --sort=size $_eflags" "ls -hS" % -la $argv
 end
