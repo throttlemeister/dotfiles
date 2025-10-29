c20dfd3b12ff6a810616370427eb7aefa431875d
throttlemeister <throttlemeister@sport-touring.eu>
2025-10-29 23:50:12 +0100

    Fix bug - missing flag for the progress bar when using advcp

--- fish/dot-config/fish/functions/cp.fish
+++ fish/dot-config/fish/functions/cp.fish
@@ -1,3 +1,3 @@
 function cp --wraps=advcp --wraps=cp -d 'Use advcp instead of cp when possible'
-    __fish_dynamic_alias cp advcp cp % -p $argv
+    __fish_dynamic_alias cp "advcp -g" cp % -p $argv
 end
