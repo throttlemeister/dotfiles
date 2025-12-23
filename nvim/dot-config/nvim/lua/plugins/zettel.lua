33b02b997906c4c53c1b9106586ee4174394a0bd
throttlemeister <throttlemeister@sport-touring.eu>
2025-12-24 00:50:29 +0100

    Added zettel to neovim for easier / better note taking

---
+++ nvim/dot-config/nvim/lua/plugins/zettel.lua
@@ -0,0 +1,13 @@
+return {
+  "breiting/zettel.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    require("zettel").setup({
+      vault_dir = "~/notes/",
+      note_types = { "note", "journal", "meeting", "meta" },
+    })
+  end,
+}
