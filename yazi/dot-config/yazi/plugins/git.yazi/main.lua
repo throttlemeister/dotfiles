5207e0b1fdaff0b650b25b428e5b1feaa073ba48
throttlemeister <throttlemeister@sport-touring.eu>
2026-01-01 19:37:17 +0100

    Fixes

--- yazi/dot-config/yazi/plugins/git.yazi/main.lua
+++ yazi/dot-config/yazi/plugins/git.yazi/main.lua
@@ -1,4 +1,4 @@
---- @since 25.5.31
+--- @since 25.12.29
 
 local WINDOWS = ya.target_family() == "windows"
 
@@ -125,12 +125,7 @@ local add = ya.sync(function(st, cwd, repo, changed)
 			st.repos[repo][path] = code
 		end
 	end
-	-- TODO: remove this
-	if ui.render then
-		ui.render()
-	else
-		ya.render()
-	end
+	ui.render()
 end)
 
 ---@param cwd string
@@ -142,12 +137,7 @@ local remove = ya.sync(function(st, cwd)
 		return
 	end
 
-	-- TODO: remove this
-	if ui.render then
-		ui.render()
-	else
-		ya.render()
-	end
+	ui.render()
 	st.dirs[cwd] = nil
 	if not st.repos[repo] then
 		return
@@ -189,6 +179,10 @@ local function setup(st, opts)
 	}
 
 	Linemode:children_add(function(self)
+		if not self._file.in_current then
+			return ""
+		end
+
 		local url = self._file.url
 		local repo = st.dirs[tostring(url.base or url.parent)]
 		local code
