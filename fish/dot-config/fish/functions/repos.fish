ae995eff58dc9240712721bf3e21523cc6047d50
throttlemeister <throttlemeister@sport-touring.eu>
2026-02-24 23:20:53 +0100

    If you do make a change, make all changes

--- fish/dot-config/fish/functions/repos.fish
+++ fish/dot-config/fish/functions/repos.fish
@@ -15,7 +15,7 @@ function repos -d "This function will synchornize all git repositories"
     echo -ne "Pulling wallpaper           4/6[███████████████████████████..............] \r"
     cd ~/Pictures/wallpapers/
     pull >/dev/null 2>&1
-    echo -ne "Pulling crashdot.com        5/6[██████████████████████████████████.......] \r"
+    echo -ne "Pulling gvdtech.net         5/6[██████████████████████████████████.......] \r"
     cd ~/web/gvdtech.net/
     pull >/dev/null 2>&1
     echo -ne "Pulling sport-touring.eu    6/6[█████████████████████████████████████████] \r"
