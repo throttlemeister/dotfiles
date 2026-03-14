637655958b455df21c955e1e262d28bc744cc187
throttlemeister <throttlemeister@sport-touring.eu>
2026-03-14 22:22:02 +0100

    Small change so I don't have to manually add '-t fish' if I want to connect to my main desktop.

--- fish/dot-config/fish/functions/ssh.fish
+++ fish/dot-config/fish/functions/ssh.fish
@@ -1,7 +1,15 @@
 function ssh -d "ssh using kitten when in kitty terminal"
-    if test -n "$KITTY_WINDOW_ID"
-        kitten ssh $argv
+    if test $argv = "thor.gvdtech.net" || test $argv = thor
+        if test -n "$KITTY_WINDOW_ID"
+            kitten ssh $argv -t fish
+        else
+            command ssh $argv -t fish
+        end
     else
-        command ssh $argv
+        if test - "$KITTY_WINDOW_ID"
+            kitten ssh $argv
+        else
+            command ssh $argv
+        end
     end
 end
