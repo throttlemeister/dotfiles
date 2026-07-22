ab09245d1b21c167d61c4becbea8bf0cb42bffab
throttlemeister <throttlemeister@sport-touring.eu>
2026-07-22 11:34:18 +0200

    Updates

--- scripts/scripts/conky-start-desktop.sh
+++ scripts/scripts/conky-start-desktop.sh
@@ -3,6 +3,6 @@
 # Start all conky instances
 #
 # 1. SysInfo conky
-/usr/bin/conky --daemonize --pause=1 -c ~/.config/conky/conky.conf
+#/usr/bin/conky --daemonize --pause=1 -c ~/.config/conky/conky.conf
 # 2. Time/Date conky
 /usr/bin/conky --daemonize --pause=1 -c ~/.config/conky/Gotham/conkyrc
