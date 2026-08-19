cbfb35bc70cba89a481e22c9d47f319c54b1ed7a
throttlemeister <throttlemeister@sport-touring.eu>
2026-08-20 01:25:57 +0200

    Changed askpass location to ~/.local/bin/

--- fish/dot-config/fish/functions/askpass.fish
+++ fish/dot-config/fish/functions/askpass.fish
@@ -1,9 +1,9 @@
 function askpass -d "installs and wraps askpass for 1P integration"
-    if type -q /usr/local/bin/askpass
+    if type -q $HOME/.local/bin/askpass
         command askpass $argv
     else
         set uhome $HOME
         echo "askpass not present... setting up"
-        sudo stow -d $uhome/.dotfiles/local/ -t /usr/local/bin askpass
+        sudo stow -d $uhome/.dotfiles/local/ -t $uhome/.local/bin askpass
     end
 end
